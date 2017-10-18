4a1bd36e25489fa0f2f826a782ab2e561618f8f3
Luis Manuel Ramirez Vargas <luis.manuel.ramirez.91@gmail.com>
2017-10-17 22:05:36 -0500

    Initial Commit

---
+++ Jarvis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jarvis
+//
+//  Created by Luis Manuel Ramirez Vargas on 17/10/17.
+//  Copyright © 2017 Luis Manuel Ramirez Vargas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
